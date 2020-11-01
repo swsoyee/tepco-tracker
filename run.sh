6e0fcbf6ffcf5b9075eff881c3e5d6eaf04cb39d
swsoyee <swsoyee@gmail.com>
2020-11-01 22:50:37 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ date_filename=$(date '+%Y%m%d')
 pip install requests
 python3 tepco-watt-stats.py $date -u $USER -p $PASSWORD > $date_filename.csv
 
+cat $date_filename.csv
 # sed 's/\"//g' $date_filename.csv | awk 'NF' | cut -f5- -d, | sed '1d' >> tepco-hourly.csv
