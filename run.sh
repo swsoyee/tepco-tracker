9de1a037645c009738d2b3cde9d1f5672699be97
swsoyee <swsoyee@gmail.com>
2020-11-01 22:32:37 +0900

    add(runner): add bash script for update data

---
+++ run.sh
@@ -0,0 +1,7 @@
+# put current date as yyyy-mm-dd in $date and $date_filename
+date=$(date '+%Y-%m-%d')
+date_filename=$(date '+%Y%m%d')
+
+python3 tepco-watt-stats.py $date -u $USER -p $PASSWORD > $date_filename.csv
+
+# sed 's/\"//g' $date_filename.csv | awk 'NF' | cut -f5- -d, | sed '1d' >> tepco-hourly.csv
