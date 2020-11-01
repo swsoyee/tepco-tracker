95890092c252752a85b111720ed4e1ef675cb0bb
swsoyee <swsoyee@gmail.com>
2020-11-02 00:31:16 +0900

    feat(drive): download all data file from drive

--- run.sh
+++ run.sh
@@ -5,5 +5,7 @@ date_filename=$(date '+%Y%m%d')
 pip install requests
 python3 tepco-watt-stats.py $date -u $USER -p $PASSWORD > $date_filename.csv
 
-cat $date_filename.csv
+pip install pydrive
+echo client_config_backend: settings'\n'client_config:'\n  'client_id: $CLIENT_ID'\n  'client_secret: $CLIENT_SECRET > settings.yaml
+python3 google-drive-downloader.py -i $RESULT_FOLDER_ID
 # sed 's/\"//g' $date_filename.csv | awk 'NF' | cut -f5- -d, | sed '1d' >> tepco-hourly.csv
