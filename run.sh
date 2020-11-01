38f14a5c7e9aeb10e7cf721c2722faa887ad28b0
swsoyee <swsoyee@gmail.com>
2020-11-02 00:39:27 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ pip3 install requests
 python3 tepco-watt-stats.py $date -u $USER -p $PASSWORD > $date_filename.csv
 
 pip3 install pydrive
+pip3 install setuptools
 echo client_config_backend: settings'\n'client_config:'\n  'client_id: $CLIENT_ID'\n  'client_secret: $CLIENT_SECRET > settings.yaml
 python3 google-drive-downloader.py -i $RESULT_FOLDER_ID
 # sed 's/\"//g' $date_filename.csv | awk 'NF' | cut -f5- -d, | sed '1d' >> tepco-hourly.csv
