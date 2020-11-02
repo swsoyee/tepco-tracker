03601460d9dab8213756596cd62296771b0edc6e
swsoyee <swsoyee@gmail.com>
2020-11-02 19:33:01 +0900

    add: yaml template

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ pip3 install requests
 
 python3 tepco-watt-stats.py $date -u $USER -p $PASSWORD > $date_filename.csv
 
-echo client_config_backend: settings'\n'client_config:'\n  'client_id: $CLIENT_ID'\n  'client_secret: $CLIENT_SECRET > settings.yaml
+echo client_config:'\n  'client_id: $CLIENT_ID'\n  'client_secret: $CLIENT_SECRET >> settings.yaml
 python3 google-drive-downloader.py -i $RESULT_FOLDER_ID
 # sed 's/\"//g' $date_filename.csv | awk 'NF' | cut -f5- -d, | sed '1d' >> tepco-hourly.csv
